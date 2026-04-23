b4888e33028c07dfe612591e1dd2033533a1a6ed
Hassan Amr <hassan122009pro@gmail.com>
2026-04-23 22:54:40 +0200

    Add files via upload

--- data.js
+++ data.js
@@ -1,30 +1,61 @@
 const siteProducts = [
     {
-        "title": "قصص الانبياء",
+        "title": "قصص الأنبياء",
         "price": "99",
         "oldPrice": "385",
-        "img": "images/Gemini_Generated_Image_etpxkuetpxkuetpx.png",
-        "category": "99 جنيها"
+        "img": "images/Gemini_Generated_Image_gtpjqogtpjqogtpj.png",
+        "category": "Silver - الباقة الفضية",
+        "desc": "ستكشف رحلة الإيمان عبر العصور. نقدم لك قصص الأنبياء والرسل بأسلوب ممتع وموثوق، لتستلهم منها العبر والقيم النبيلة وتضيء بها مسار حياتك"
     },
     {
-        "title": "بللايرس",
-        "price": "280",
-        "oldPrice": "555",
-        "img": "images/668859758_122158718216959194_8076246516627778159_n.jpg",
-        "category": "يبرس"
+        "title": "قصة سيدنا موسي",
+        "price": "99",
+        "oldPrice": "385",
+        "img": "images/Gemini_Generated_Image_exwupfexwupfexwu.png",
+        "category": "Silver - الباقة الفضية",
+        "description": ""
     },
     {
-        "title": "فتغثلص",
-        "price": "154",
-        "oldPrice": "63",
-        "img": "images/logo.jpg",
-        "category": "99 جنيها"
+        "title": "تعليم القرآن للأطفال جزء عم بالتكرار",
+        "price": "99",
+        "oldPrice": "385",
+        "img": "images/Gemini_Generated_Image_ebltlkebltlkeblt.png",
+        "category": "Silver - الباقة الفضية",
+        "description": ""
     }
 ];
 
 const siteReviews = [
     {
-        "name": "أم أحمد",
-        "img": "images/review1.jpg"
+        "name": "",
+        "img": "images/669077075_122158718144959194_6607625318423325678_n.jpg"
+    },
+    {
+        "name": "",
+        "img": "images/669565106_122158718036959194_1415701730434885706_n.jpg"
+    },
+    {
+        "name": "",
+        "img": "images/669582877_122158718108959194_6812810484143895248_n.jpg"
+    },
+    {
+        "name": "",
+        "img": "images/669849426_122158718258959194_5423148741271652627_n.jpg"
+    },
+    {
+        "name": "",
+        "img": "images/670484468_122158718294959194_765857313705263315_n.jpg"
+    },
+    {
+        "name": "",
+        "img": "images/668723731_122158718330959194_2558354002896951775_n.jpg"
+    },
+    {
+        "name": "",
+        "img": "images/668806651_122158718072959194_9214717764437013833_n.jpg"
+    },
+    {
+        "name": "",
+        "img": "images/669077075_122158718144959194_6607625318423325678_n.jpg"
     }
 ];
