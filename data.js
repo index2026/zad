3fe7f481feb0158351da3c52e42df6db3b76695d
index2026 <hassan122009pro@gmail.com>
2026-04-23 03:10:40 +0200

    Add files via upload

---
+++ data.js
@@ -0,0 +1,30 @@
+const siteProducts = [
+    {
+        "title": "قصص الانبياء",
+        "price": "99",
+        "oldPrice": "385",
+        "img": "images/Gemini_Generated_Image_etpxkuetpxkuetpx.png",
+        "category": "99 جنيها"
+    },
+    {
+        "title": "بللايرس",
+        "price": "280",
+        "oldPrice": "555",
+        "img": "images/668859758_122158718216959194_8076246516627778159_n.jpg",
+        "category": "يبرس"
+    },
+    {
+        "title": "فتغثلص",
+        "price": "154",
+        "oldPrice": "63",
+        "img": "images/logo.jpg",
+        "category": "99 جنيها"
+    }
+];
+
+const siteReviews = [
+    {
+        "name": "أم أحمد",
+        "img": "images/review1.jpg"
+    }
+];
